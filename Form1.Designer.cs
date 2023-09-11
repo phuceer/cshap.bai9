f30f9f36b639c8c32eb974ba47efccb1aadadc2a
ADMIN <ADMIN@LAB1-MAY32>
2023-09-11 16:33:46 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace cshap.bai9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblname = new System.Windows.Forms.TextBox();
+            this.lblram = new System.Windows.Forms.TextBox();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(102, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(627, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "chương trình lấy thông tin máy tính";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(73, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 46);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(73, 246);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 46);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "ram:";
+            // 
+            // lblname
+            // 
+            this.lblname.Location = new System.Drawing.Point(223, 156);
+            this.lblname.Name = "lblname";
+            this.lblname.Size = new System.Drawing.Size(367, 20);
+            this.lblname.TabIndex = 3;
+            // 
+            // lblram
+            // 
+            this.lblram.Location = new System.Drawing.Point(223, 269);
+            this.lblram.Name = "lblram";
+            this.lblram.Size = new System.Drawing.Size(367, 20);
+            this.lblram.TabIndex = 4;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(271, 356);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(160, 47);
+            this.btnThoat.TabIndex = 5;
+            this.btnThoat.Text = "thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.lblram);
+            this.Controls.Add(this.lblname);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox lblname;
+        private System.Windows.Forms.TextBox lblram;
+        private System.Windows.Forms.Button btnThoat;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cshap.bai9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            long DungLuongRAM = 0;
+            lblname.Text = Environment.MachineName.ToString();
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher
+            ("SELECT * FROM Win32_Computersystem");
+            foreach (ManagementObject obj in searcher.Get())
+            {
+                DungLuongRAM = long.Parse(obj["TotalPhysicalMemory"].ToString());
+                DungLuongRAM = DungLuongRAM / (1024 * 1024); 
+            }
+            lblram.Text = DungLuongRAM.ToString() + " GB";
+        }
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            Close();
+
+        }
+    }
+}
